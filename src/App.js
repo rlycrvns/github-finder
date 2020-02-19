3b5fa188aebac9c5b2ff700d131a44d4caf6bcac
rlycrvns <rlycrvns@gmail.com>
2020-02-19 13:03:21 -0800

    added clear function

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ class App extends Component {
   // }
 
   searchUsers = async text => {
-    this.setState({ loading: true});
+    this.setState({ loading: true });
     const res = await axios.get(
       `https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}
       &client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
@@ -30,12 +30,14 @@ class App extends Component {
     this.setState({ users: res.data.items, loading: false });
   };
 
+  clearUsers = () => this.setState({ users: [], loading: false } );
+
   render() {
     return (
       <div className="App">
         <Navbar />
         <div className="container">
-          <Search searchUsers={this.searchUsers} />
+          <Search searchUsers={this.searchUsers} clearUsers={this.clearUsers} />
           <Users loading={this.state.loading} users={this.state.users} />
         </div>
       </div>
--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,6 +1,5 @@
-import React, { Component } from "react"
-import PropTypes from 'prop-types'
-
+import React, { Component } from "react";
+import PropTypes from "prop-types";
 
 class Search extends Component {
   state = {
@@ -8,13 +7,14 @@ class Search extends Component {
   };
 
   static propTypes = {
-    searchUsers: PropTypes.func.isRequired
+    searchUsers: PropTypes.func.isRequired,
+    clearUsers: PropTypes.func.isRequired
   };
 
   onSubmit = e => {
     e.preventDefault();
     this.props.searchUsers(this.state.text);
-    this.setState({ text: '' });
+    this.setState({ text: "" });
   };
 
   onChange = e => this.setState({ [e.target.name]: e.target.value });
@@ -36,6 +36,12 @@ class Search extends Component {
             className="btn btn-dark btn-block"
           />
         </form>
+        <button
+          className="btn btn-light btn-block"
+          onClick={this.props.clearUsers}
+        >
+          Clear
+        </button>
       </div>
     );
   }
