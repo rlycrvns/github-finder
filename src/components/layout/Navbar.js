1fe4bb821ee153c4c60808b75e6352af10ce00a8
rlycrvns <rlycrvns@gmail.com>
2020-02-26 14:23:41 -0800

    added user component

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,5 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 
 const Navbar = ({ title, icon }) => {
   return (
@@ -7,14 +8,22 @@ const Navbar = ({ title, icon }) => {
       <h1>
         <i className={icon}></i> {title}
       </h1>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+      </ul>
     </nav>
   );
 };
 
 Navbar.defaultProps = {
-  title: 'Github Finder',
-  icon: 'fab fa-github'
-}
+  title: "Github Finder",
+  icon: "fab fa-github"
+};
 
 Navbar.propTypes = {
   title: PropTypes.string.isRequired,
