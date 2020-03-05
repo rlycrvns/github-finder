101fae78db33ec6cd5aaa057581e556bf24cf5b0
Riley Cravens <rlycrvns@gmail.com>
2020-03-04 17:28:27 -0800

    fixed typo

--- src/App.js
+++ src/App.js
@@ -18,16 +18,19 @@ const App = () => {
 
   const searchUsers = async text => {
     setLoading(true);
+
     const res = await axios.get(
       `https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}
       &client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
     );
-    setUsers(res.date.items);
+
+    setUsers(res.data.items);
     setLoading(false);
   };
 
   const getUser = async username => {
     setLoading(true);
+
     const res = await axios.get(
       `https://api.github.com/users/${username}?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}
       &client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
@@ -39,6 +42,7 @@ const App = () => {
 
   const getUserRepos = async username => {
     setLoading(true);
+
     const res = await axios.get(
       `https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}
       &client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
