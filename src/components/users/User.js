e4db48bcbc88e971f5e53524a0016093562ab3c9
Riley Cravens <rlycrvns@gmail.com>
2020-03-04 16:44:57 -0800

    fixed useffect

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,14 +1,14 @@
-import React, { Fragment, useEffect } from "react";
-import PropTypes from "prop-types";
-import Repos from "../repos/Repos";
-import Spinner from "../layout/Spinner";
-import { Link } from "react-router-dom";
+import React, { Fragment, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import Repos from '../repos/Repos';
+import Spinner from '../layout/Spinner';
+import { Link } from 'react-router-dom';
 
 const User = ({ user, loading, getUser, getUserRepos, repos, match }) => {
   useEffect(() => {
     getUser(match.params.login);
     getUserRepos(match.params.login);
-  });
+  }, []);
 
   const {
     name,
@@ -30,22 +30,22 @@ const User = ({ user, loading, getUser, getUserRepos, repos, match }) => {
 
   return (
     <Fragment>
-      <Link to="/" className="btn btn-light">
+      <Link to='/' className='btn btn-light'>
         Back to Search
       </Link>
-      Hireable:{" "}
+      Hireable:{' '}
       {hireable ? (
-        <i className="fas fa-check text-success" />
+        <i className='fas fa-check text-success' />
       ) : (
-        <i className="fas fa-times-circle text-danger" />
+        <i className='fas fa-times-circle text-danger' />
       )}
-      <div className="card grid-2">
-        <div className="all-center">
+      <div className='card grid-2'>
+        <div className='all-center'>
           <img
             src={avatar_url}
-            alt=""
-            className="round-img"
-            style={{ width: "150px" }}
+            alt=''
+            className='round-img'
+            style={{ width: '150px' }}
           />
           <h1>{name}</h1>
           <p>Location: {location}</p>
@@ -57,7 +57,7 @@ const User = ({ user, loading, getUser, getUserRepos, repos, match }) => {
               <p>{bio}</p>
             </Fragment>
           )}
-          <a href={html_url} className="btn btn-dark my-1">
+          <a href={html_url} className='btn btn-dark my-1'>
             Visit Github Profile
           </a>
           <ul>
@@ -85,11 +85,11 @@ const User = ({ user, loading, getUser, getUserRepos, repos, match }) => {
           </ul>
         </div>
       </div>
-      <div className="card text-center">
-        <div className="badge badge-primary">Followers: {followers}</div>
-        <div className="badge badge-light">Following: {following}</div>
-        <div className="badge badge-success">Public Repos: {public_repos}</div>
-        <div className="badge badge-dark">Public Gists: {public_gists}</div>
+      <div className='card text-center'>
+        <div className='badge badge-primary'>Followers: {followers}</div>
+        <div className='badge badge-light'>Following: {following}</div>
+        <div className='badge badge-success'>Public Repos: {public_repos}</div>
+        <div className='badge badge-dark'>Public Gists: {public_gists}</div>
       </div>
       <Repos repos={repos} />
     </Fragment>
