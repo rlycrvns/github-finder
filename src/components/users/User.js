7dbe949fa0ab53dd017860b2b125e93e64664e8e
rlycrvns <rlycrvns@gmail.com>
2020-08-20 11:59:20 -0700

    refactor: using context

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,10 +1,14 @@
-import React, { Fragment, useEffect } from 'react';
-import PropTypes from 'prop-types';
-import Repos from '../repos/Repos';
-import Spinner from '../layout/Spinner';
-import { Link } from 'react-router-dom';
+import React, { Fragment, useEffect, useContext } from "react";
+import Repos from "../repos/Repos";
+import Spinner from "../layout/Spinner";
+import { Link } from "react-router-dom";
+import GithubContext from "../../context/github/githubContext";
 
-const User = ({ user, loading, getUser, getUserRepos, repos, match }) => {
+const User = ({ match }) => {
+  const githubContext = useContext(GithubContext);
+
+  const { getUser, loading, user, repos, getUserRepos } = githubContext;
+
   useEffect(() => {
     getUser(match.params.login);
     getUserRepos(match.params.login);
@@ -24,29 +28,29 @@ const User = ({ user, loading, getUser, getUserRepos, repos, match }) => {
     following,
     public_repos,
     public_gists,
-    hireable
+    hireable,
   } = user;
 
   if (loading) return <Spinner />;
 
   return (
     <Fragment>
-      <Link to='/' className='btn btn-light'>
+      <Link to="/" className="btn btn-light">
         Back to Search
       </Link>
-      Hireable:{' '}
+      Hireable:{" "}
       {hireable ? (
-        <i className='fas fa-check text-success' />
+        <i className="fas fa-check text-success" />
       ) : (
-        <i className='fas fa-times-circle text-danger' />
+        <i className="fas fa-times-circle text-danger" />
       )}
-      <div className='card grid-2'>
-        <div className='all-center'>
+      <div className="card grid-2">
+        <div className="all-center">
           <img
             src={avatar_url}
-            alt=''
-            className='round-img'
-            style={{ width: '150px' }}
+            alt=""
+            className="round-img"
+            style={{ width: "150px" }}
           />
           <h1>{name}</h1>
           <p>Location: {location}</p>
@@ -58,7 +62,7 @@ const User = ({ user, loading, getUser, getUserRepos, repos, match }) => {
               <p>{bio}</p>
             </Fragment>
           )}
-          <a href={html_url} className='btn btn-dark my-1'>
+          <a href={html_url} className="btn btn-dark my-1">
             Visit Github Profile
           </a>
           <ul>
@@ -86,23 +90,15 @@ const User = ({ user, loading, getUser, getUserRepos, repos, match }) => {
           </ul>
         </div>
       </div>
-      <div className='card text-center'>
-        <div className='badge badge-primary'>Followers: {followers}</div>
-        <div className='badge badge-light'>Following: {following}</div>
-        <div className='badge badge-success'>Public Repos: {public_repos}</div>
-        <div className='badge badge-dark'>Public Gists: {public_gists}</div>
+      <div className="card text-center">
+        <div className="badge badge-primary">Followers: {followers}</div>
+        <div className="badge badge-light">Following: {following}</div>
+        <div className="badge badge-success">Public Repos: {public_repos}</div>
+        <div className="badge badge-dark">Public Gists: {public_gists}</div>
       </div>
       <Repos repos={repos} />
     </Fragment>
   );
-};
-
-User.propTypes = {
-  loading: PropTypes.bool,
-  user: PropTypes.object.isRequired,
-  repos: PropTypes.array.isRequired,
-  getUser: PropTypes.func.isRequired,
-  getUserRepos: PropTypes.func.isRequired
 };
 
 export default User;
