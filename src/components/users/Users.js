942a0768325b710e503b04f5715b12624a457ee1
rlycrvns <rlycrvns@gmail.com>
2020-08-20 11:42:02 -0700

    drop: propTypes

--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,7 +1,6 @@
 import React, {useContext } from "react";
 import UserItem from "./UserItem";
 import Spinner from "../layout/Spinner";
-import PropTypes from 'prop-types'
 import GithubContext from '../../context/github/githubContext';
 
 
@@ -23,10 +22,6 @@ const { loading, users } = githubContext;
   }
 }
 
-Users.propTypes = {
-  users: PropTypes.array.isRequired,
-  loading: PropTypes.bool.isRequired
-}
 const userStyle = {
   display: "grid",
   gridTemplateColumns: "repeat(3, 1fr)",
