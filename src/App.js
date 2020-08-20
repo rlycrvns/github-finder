25972bd877c4d5a8c63f9d490edce411f8a5d80e
rlycrvns <rlycrvns@gmail.com>
2020-08-20 11:58:47 -0700

    drop: getUserRepos func

--- src/App.js
+++ src/App.js
@@ -6,25 +6,11 @@ import User from "./components/users/User";
 import Search from "./components/users/Search";
 import Alert from "./components/layout/Alert";
 import About from "./components/pages/About";
-import axios from "axios";
 import GithubState from "./context/github/GithubState";
 import "./App.css";
 
 const App = () => {
-  const [repos, setRepos] = useState([]);
   const [alert, setAlert] = useState(null);
-
-  const getUserRepos = async (username) => {
-    setLoading(true);
-
-    const res = await axios.get(
-      `https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}
-      &client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
-    );
-
-    setRepos(res.data);
-    setLoading(false);
-  };
 
   const showAlert = (msg, type) => {
     setAlert({ msg, type });
@@ -45,25 +31,13 @@ const App = () => {
                 path="/"
                 render={(props) => (
                   <Fragment>
-                    <Search
-                      setAlert={showAlert}
-                    />
+                    <Search setAlert={showAlert} />
                     <Users />
                   </Fragment>
                 )}
               />
               <Route exact path="/about" component={About} />
-              <Route
-                exact
-                path="/user/:login"
-                render={(props) => (
-                  <User
-                    {...props}
-                    getUserRepos={getUserRepos}
-                    repos={repos}
-                  />
-                )}
-              />
+              <Route exact path="/user/:login" component={User} />
             </Switch>
           </div>
         </div>
