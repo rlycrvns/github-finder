1a8cc14e21f8692ff175af23b97ebfb79336bca2
rlycrvns <rlycrvns@gmail.com>
2020-08-20 11:41:14 -0700

    drop: getUser function

--- src/App.js
+++ src/App.js
@@ -11,23 +11,10 @@ import GithubState from "./context/github/GithubState";
 import "./App.css";
 
 const App = () => {
-  const [users, setUsers] = useState([]);
   const [user, setUser] = useState({});
   const [repos, setRepos] = useState([]);
   const [loading, setLoading] = useState(false);
   const [alert, setAlert] = useState(null);
-
-  const getUser = async (username) => {
-    setLoading(true);
-
-    const res = await axios.get(
-      `https://api.github.com/users/${username}?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}
-      &client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
-    );
-
-    setUser(res.data);
-    setLoading(false);
-  };
 
   const getUserRepos = async (username) => {
     setLoading(true);
