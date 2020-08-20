8a32891ff895b8086c65dee21d96bb122d2ac3f6
rlycrvns <rlycrvns@gmail.com>
2020-08-20 12:00:00 -0700

    add: getUserRepos

--- src/context/github/GithubState.js
+++ src/context/github/GithubState.js
@@ -46,11 +46,24 @@ const GithubState = (props) => {
 
     dispatch({
       type: GET_USER,
-      payload: res.data
-    })
+      payload: res.data,
+    });
   };
 
   //Get Repos
+  const getUserRepos = async (username) => {
+    setLoading();
+
+    const res = await axios.get(
+      `https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}
+      &client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+
+    dispatch({
+      type: GET_REPOS,
+      payload: res.data,
+    });
+  };
 
   // Clear Users
   const clearUsers = () => dispatch({ type: CLEAR_USERS });
@@ -66,7 +79,9 @@ const GithubState = (props) => {
         repos: state.repos,
         loading: state.loading,
         searchUsers,
-        clearUsers
+        clearUsers,
+        getUser,
+        getUserRepos,
       }}
     >
       {props.children}
